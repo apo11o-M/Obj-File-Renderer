aaf313e912fc752cb8efbccb51d1416da539c7f2
RiiickWang <54286072+apo11o-M@users.noreply.github.com>
2021-11-19 03:28:37 -0800

    fixed memory leak in the pixel arrays

--- Main.cpp
+++ Main.cpp
@@ -26,8 +26,8 @@ int main() {
     cout << "\nProgram Start.." << endl;
 
     // The width and height of the window
-    // const int width = 1000, height = 700;
-    const int width = 750, height = 500;
+    const int width = 1000, height = 700;
+    // const int width = 750, height = 500;
     // The rotation rate at each axis in degrees, default is 0
     float rotX = 0, rotY = 0, rotZ = 0;
     // The distance between the object and the viewer
@@ -46,14 +46,14 @@ int main() {
     int framerate = 144;
     // whether or not pause the game
     bool pause = true;
-    // Keypress boolean 
+    // Keypress boolean
     bool right = false, left = false, up = false, down = false;
     // Smooth angle threshold
     float smoothAngle = 15;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(width, height), "Obj File Renderer", 
+    sf::RenderWindow window(sf::VideoMode(width, height), "Obj File Renderer",
                                           sf::Style::Default, settings);
     window.setFramerateLimit(framerate);
 
@@ -89,7 +89,7 @@ int main() {
     // The buffer vertices
     vector<Vec3d> vertices = block.getVert();
     // The buffer vertex normals
-    vector<Vec3d> vertNorm = block.getVertNorm(); 
+    vector<Vec3d> vertNorm = block.getVertNorm();
     // The buffer surface normals
     vector<Vec3d> surfNorm = block.getSurfNorm();
     // The buffer position of the surfNorm vertices
@@ -102,7 +102,7 @@ int main() {
     vector<Vec3d> facingSurfNormPos;
 
     // The final buffer that stores the color of the faces that are facing the camera
-    vector<Color> facingVertColor; 
+    vector<Color> facingVertColor;
     // The buffer coordinates of the model on a 2D screen.
     vector<Vec2d> screenVert;
 
@@ -111,12 +111,12 @@ int main() {
         cout << "Error creating the texture" << endl;
         return -1;
     }
-    texture.setSmooth(true);    
+    texture.setSmooth(true);
     sf::Sprite sprite(texture);
+    vector<Uint8> pixels(width * height * 4);
 
     // start the game loop
     while (window.isOpen()) {
-        Uint8* pixels = new Uint8[width * height * 4];
 
         // Event handlers
         sf::Event event;
@@ -207,7 +207,7 @@ int main() {
             screenVert.push_back(facingCamVerts.at(i).projection(k1, k2, width, height));
         }
 
-        // Goes through every single triangle, look through every pixel that is enclosed by the 
+        // Goes through every single triangle, look through every pixel that is enclosed by the
         // rectangle (which also encloses the triangle) and use the shoelace method to calculate the
         // weight of the normal and the position at each vertices to the specific pixel.
         for (int i = 0; i < screenVert.size(); i += 3) {
@@ -215,11 +215,11 @@ int main() {
             Vec2d B = screenVert.at(i + 1);
             Vec2d C = screenVert.at(i + 2);
             int yMin = std::min({A.y, B.y, C.y});       // Here are the boundaries of the rectangle
-            int yMax = std::max({A.y, B.y, C.y});       // that encloses the triangle 
+            int yMax = std::max({A.y, B.y, C.y});       // that encloses the triangle
             int xMin = std::min({A.x, B.x, C.x});
             int xMax = std::max({A.x, B.x, C.x});
 
-            for (int y = yMin; y <= yMax; y++) {        // Calculate the lighting for each pixel in  
+            for (int y = yMin; y <= yMax; y++) {        // Calculate the lighting for each pixel in
                 for (int x = xMin; x <= xMax; x++) {    // the enclosed rectangle
                     int index = coordToIndex(x, y, width, height);
                     if (index != -1) {
@@ -251,10 +251,11 @@ int main() {
                         }
                     }
                 }
-            }        
+            }
         }
 
-        texture.update(pixels);
+        texture.update(&pixels[0]);
+
         window.clear(Color::Black);
         window.draw(sprite);
         window.draw(textInstruction);
@@ -267,7 +268,7 @@ int main() {
         facingCamVerts.clear();
         facingVertColor.clear();
         screenVert.clear();
-        delete[] pixels;
+        std::fill(pixels.begin(), pixels.end(), 0);
 
         // Update the rotation angle
         if (!pause) {
